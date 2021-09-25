f12003443099c717994ff937012065adb1a02e75
Phelipe Wener <phwener@gmail.com>
2021-09-24 23:20:57 -0300

    definitive solution to climbing the leaderboard

--- climbing_the_leaderboard.go
+++ climbing_the_leaderboard.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
 )
 
 /*
@@ -12,50 +17,115 @@ import (
  *  1. INTEGER_ARRAY ranked
  *  2. INTEGER_ARRAY player
  */
-func climbingLeaderboard(ranked []int32, player []int32) []int32 {
-	playerRanks := []int32{}
+ func climbingLeaderboard(ranked []int32, player []int32) []int32 {
+    playerRanks := []int32{}
 
-	var currentRank int32 = 1
-	rankIndex := 0
+    var currentRank int32 = 1
+    rankIndex := 0
 
     lastScore := ranked[len(ranked)-1]
-	lastScoreIndex := len(player) - 1
+    lastScoreIndex := len(player) - 1
 
-	for lastScoreIndex >= 0 {
-		rankScore := ranked[rankIndex]
+    for lastScoreIndex >= 0 {
+        rankScore := ranked[rankIndex]
 
-		if player[lastScoreIndex] >= rankScore {
+        if player[lastScoreIndex] >= rankScore {
             playerRanks = append(playerRanks, currentRank)
             lastScoreIndex--;
-		}
+            continue
+        }
 
-		if rankIndex == len(ranked) - 1 {
-			playerRanks = append(playerRanks, currentRank+1)
+        if rankIndex == (len(ranked) - 1) {
+            diff := len(player) - len(playerRanks)
+            for i := 0; i < diff; i++ {
+                playerRanks = append(playerRanks, currentRank+1)
+            }
             break;
-		}
+        }
 
-		if rankScore != lastScore {
-			currentRank++
-		}
+        if rankScore != lastScore {
+            currentRank++
+        }
 
-		rankIndex++
+        rankIndex++
 
-		lastScore = rankScore
-	}
+        lastScore = rankScore
+    }
 
-	return reverseInts(playerRanks)
+    return reverseInts(playerRanks)
 }
 
 func reverseInts(input []int32) []int32 {
-	if len(input) == 0 {
-		return input
-	}
-	return append(reverseInts(input[1:]), input[0])
+    if len(input) == 0 {
+        return input
+    }
+    return append(reverseInts(input[1:]), input[0])
 }
 
 func main() {
-	input := []int32{100, 90, 90, 80, 75, 60}
-	score := []int32{50, 65, 77, 90, 102}
+    reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
 
-	fmt.Println(climbingLeaderboard(input, score))
+    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+    checkError(err)
+
+    defer stdout.Close()
+
+    writer := bufio.NewWriterSize(stdout, 16 * 1024 * 1024)
+
+    rankedCount, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+    checkError(err)
+
+    rankedTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+    var ranked []int32
+
+    for i := 0; i < int(rankedCount); i++ {
+        rankedItemTemp, err := strconv.ParseInt(rankedTemp[i], 10, 64)
+        checkError(err)
+        rankedItem := int32(rankedItemTemp)
+        ranked = append(ranked, rankedItem)
+    }
+
+    playerCount, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+    checkError(err)
+
+    playerTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+    var player []int32
+
+    for i := 0; i < int(playerCount); i++ {
+        playerItemTemp, err := strconv.ParseInt(playerTemp[i], 10, 64)
+        checkError(err)
+        playerItem := int32(playerItemTemp)
+        player = append(player, playerItem)
+    }
+
+    result := climbingLeaderboard(ranked, player)
+
+    for i, resultItem := range result {
+        fmt.Fprintf(writer, "%d", resultItem)
+
+        if i != len(result) - 1 {
+            fmt.Fprintf(writer, "\n")
+        }
+    }
+
+    fmt.Fprintf(writer, "\n")
+
+    writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
 }
