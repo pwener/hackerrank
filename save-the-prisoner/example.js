bd1cdf6d6f017bb63bb3ead7d1782dfffc80aab3
Phelipe Wener <phwener@gmail.com>
2023-06-12 23:42:14 -0300

    add day 1 - js badge mission

---
+++ save-the-prisoner/example.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'saveThePrisoner' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER m
+ *  3. INTEGER s
+ */
+function saveThePrisoner(n, m, s) {
+    const start = m + s - 1;
+    const pos = start % n;
+
+    if (pos == 0) {
+        return n
+    }
+
+    return pos;
+}
+
+function main() {
+    // const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const t = parseInt(readLine().trim(), 10);
+
+    for (let tItr = 0; tItr < t; tItr++) {
+        const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+        const n = parseInt(firstMultipleInput[0], 10);
+
+        const m = parseInt(firstMultipleInput[1], 10);
+
+        const s = parseInt(firstMultipleInput[2], 10);
+
+        const result = saveThePrisoner(n, m, s);
+
+        // ws.write(result + '\n');
+        console.log(result + '\n')
+    }
+
+    // ws.end();
+}
